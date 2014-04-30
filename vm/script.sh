ab4befb0a5e8d649b33eaf04c3e6fe0aef777824
Hadrien Lanneau <hadrien.lanneau@wikiogroup.com>
2014-04-30 17:44:28 +0200

    PhantomJS, yeoman and angular generator

--- vm/script.sh
+++ vm/script.sh
@@ -4,11 +4,11 @@
 apt-get install -y python-software-properties python g++ make
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
-apt-get install -y git tig git-gui nodejs redis-server nginx screen
+apt-get install -y git tig git-gui nodejs redis-server nginx screen phantomjs
 
 # Install node package
 npm config set registry http://registry.npmjs.org/
-npm install -g redis-commander jasmine-node
+npm install -g redis-commander jasmine-node express yo generator-angular
 
 # Set nginx config
 cp /vagrant/resources/nginx-ng-cra /etc/nginx/sites-enabled/ng-cra
